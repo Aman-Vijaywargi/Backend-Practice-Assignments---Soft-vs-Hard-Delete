1cef5d1d45a391e27ec2fe13aa012be34f3139e8
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-07-01 22:48:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,86 +1,47 @@
 const express = require('express');
 const Student = require('./models/Student');
-
-
 const app = express();
 
-// middleware 
 app.use(express.json());
-
-// Routes
-
-// Get all the students
 app.get('/students', async (req, res) => {
     // write your codes here
-    const students = await Student.find();
-    res.send(students)
+    const students = await Student.find({isDeleted:false});
+    if(!students.length) return res.sendStatus(404);
+    res.send(students);
 })
 
-// Add student to database
 app.post('/students', async (req, res) =>{
-    // write your codes here
-    if(!req.body.name || !req.body.sex || !req.body.age || 
-        !req.body.class || !req.body.grade_point ){
-            res.status(400).send({message: 'Please send appropriate data'})
-    }
-    let student = new Student({
-        name : req.body.name,
-        sex: req.body.sex,
-        age: req.body.age,
-        class: req.body.class,
-        grade_point: req.body.grade_point
-    })
-    let s  = await student.save();
-    res.send(s)
+    const student = new Student({
+        name:req.body.name,
+        sex:req.body.sex,
+        age:req.body.age,
+        class:req.body.class,
+        grade_point:req.body.grade_point
+    });
+
+    await student.save();
+    res.send(student);
 })
 
-// Get specific student
 app.get('/students/:id', async (req, res) =>{
     // write your codes here
-    try{
-        const student = await Student.findById(req.params.id);
-        if(!student){
-            res.status(404).send({message: "no longer exist/record not found"})
-        }
-        res.send(student)
-    }catch(e){
-        res.status(404).send({message:e.message})
-    }
+    const student = await Student.findById(req.params.id);
+    if(student.isDeleted) return res.sendStatus(404);
+    res.send(student);
 })
 
-// delete specific student
 app.delete('/student/:id', async (req, res) =>{
-    // write your codes here
-    try{
-        
-        if(req.query.type=="soft"){
-
-            const s = await Student.findById(req.params.id);
-            if(!s || s.isDeleted){
-                res.status(404).send(s)
-            }
-            const student = await Student.update({_id: req.params.id},{
-                $set:{
-                    isDeleted: true
-                }
-            }, {new: true}
-            )
-            res.send(student);
-            
-        }
-        if(req.query.type == "hard"){
-            const s = await Student.findById(req.params.id);
-            if(!s || s.isDeleted){
-                res.status(404).send(s)
-            }
-            const result = await Student.findByIdAndDelete(req.params.id);
-            res.send(result)
-        }
-
-    }catch(e){
-        res.send({message: e.message})
+    if(req.query.type === "soft") {
+        const student = await Student.findById(req.params.id);
+        if(student.isDeleted) return res.sendStatus(404);
+        student.isDeleted = true;
+        await student.save();
+        res.sendStatus(200);
+    }
+    if(req.query.type === "hard") {
+        await Student.deleteOne({_id:req.params.id});  
+        res.sendStatus(200);
     }
 }) 
-
 
 module.exports = app;
